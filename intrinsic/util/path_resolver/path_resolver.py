df3fdf17a68ecb11c282c2ca0a9a48a5d3eb43ea
timdn <timdn@google.com>
2025-04-16 07:07:04 -0700

    SDK update
    GitOrigin-RevId: c2606bf91680da1f5d581b78614530b523d45c4d

--- intrinsic/util/path_resolver/path_resolver.py
+++ intrinsic/util/path_resolver/path_resolver.py
@@ -9,6 +9,14 @@ from python.runfiles import runfiles
 
 def resolve_runfiles_path(path: str) -> str:
   """Returns the runfiles path for the given path."""
+  for prefix in ['_main', 'ai_intrinsic_sdks']:
+    test_path = os.path.normpath(
+        runfiles.Create().Rlocation(os.path.join(prefix, path))
+    )
+    if os.path.exists(test_path):
+      return test_path
+
+  # If we don't know any better default to the intrinsic version
   return os.path.normpath(
-      runfiles.Create().Rlocation(os.path.join('ai_intrinsic_sdks', path))
+      runfiles.Create().Rlocation(os.path.join('ai_intrinsic_sdks', path)),
   )
