55025794f185b91792e629e8f089b6395e5cfd4d
Hauke Heibel <haukeheibel@google.com>
2025-05-20 13:43:13 -0700

    SDK update
    GitOrigin-RevId: e83fe1451b97cb8a40717735db16332e6cb81c49

---
+++ intrinsic/util/grpc/grpc_utils.py
@@ -0,0 +1,101 @@
+# Copyright 2023 Intrinsic Innovation LLC
+
+"""Utility functions for gRPC connections."""
+
+from typing import Optional
+
+import grpc
+from intrinsic.config import environments  # For service discovery
+from intrinsic.kubernetes.acl.ipcidentity import ipcidentity
+from intrinsic.kubernetes.acl.ipcidentity.internal import metadata
+from intrinsic.util.grpc import interceptor
+
+
+def _get_compute_project() -> str:
+  """Gets the compute project from the metadata server.
+
+  Returns:
+    The compute project.
+  """
+  return metadata.MetadataClient().compute_project()
+
+
+def _get_environment() -> str:
+  """Gets the environment based on the compute project.
+
+  The environment is either prod, dev or staging. It is important to
+  consistently use the correct environment because each environment has
+  associated its dedicated account, assets, compute projects and virtual
+  machines.
+
+  This function is supposed to be used within a cluster and first determines the
+  compute project from the metadata server. Then it uses the compute project to
+  determine the environment.
+
+  Returns:
+    The environment based on the compute project.
+  """
+  compute_project = _get_compute_project()
+  return environments.from_compute_project(compute_project)
+
+
+def _add_auth_header(
+    channel: grpc.Channel,
+    ipc_identity: Optional[ipcidentity.IpcIdentity],
+) -> grpc.Channel:
+  """Adds an auth header to the gRPC channel."""
+  if ipc_identity:
+    auth_header = lambda: [("cookie", "auth-proxy=" + ipc_identity.token())]
+    auth_interceptor = interceptor.HeaderAdderInterceptor(auth_header)
+    return grpc.intercept_channel(channel, auth_interceptor)
+  return channel
+
+
+def _create_channel(address: str) -> grpc.Channel:
+  """Creates a gRPC channel with the given address."""
+  return grpc.secure_channel(
+      address,
+      grpc.ssl_channel_credentials(),
+      options=[("grpc.max_receive_message_length", -1)],
+  )
+
+
+def create_assets_channel(
+    ipc_identity: Optional[ipcidentity.IpcIdentity] = None,
+) -> grpc.Channel:
+  """Creates a gRPC channel for the assets service.
+
+  If an IPC identity is provided, it is used to add an auth header to the
+  channel. This can be used to authenticate requests to the assets service.
+
+  Args:
+    ipc_identity: The IPC identity to use for authentication.
+
+  Returns:
+    The gRPC channel for the assets service.
+  """
+  env = _get_environment()
+  assets_domain = environments.assets_domain(env)
+  assets_channel = _create_channel(f"{assets_domain}:443")
+  return _add_auth_header(assets_channel, ipc_identity)
+
+
+def create_cloud_channel(
+    ipc_identity: Optional[ipcidentity.IpcIdentity] = None,
+) -> grpc.Channel:
+  """Creates a gRPC channel for the cloud service.
+
+  If an IPC identity is provided, it is used to add an auth header to the
+  channel. This can be used to authenticate requests to the assets service.
+
+  Args:
+    ipc_identity: The IPC identity to use for authentication.
+
+  Returns:
+    The gRPC channel for the cloud service.
+  """
+  compute_project = _get_compute_project()
+  cloud_channel = _create_channel(
+      f"www.endpoints.{compute_project}.cloud.goog:443"
+  )
+  return _add_auth_header(cloud_channel, ipc_identity)
