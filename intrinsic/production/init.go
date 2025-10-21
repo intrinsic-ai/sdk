fb5bf8d750f6b83906c196b9e986e50519098edd
maleo <maleo@google.com>
2025-10-21 08:21:08 -0700

    SDK update
    GitOrigin-RevId: b8d96fa777ce678962e17801c4d9b456ccb27438

--- intrinsic/production/init.go
+++ intrinsic/production/init.go
@@ -10,6 +10,9 @@ import (
 // Init is the entry point for our Golang binaries. It parses command line flags and performs
 // other common initialization.
 func Init() {
+	// Avoids logging command-line args and version information, which we get via
+	// other means in our stack.
+	flag.Set("silent_init", "true")
 	// When manually running a binary, we want to see the logs on stderr.
 	// When running in k8s, we want to see them in container logs and disable writing them to a file.
 	flag.Set("logtostderr", "true")
