65da10c0ca0ec7994d8b0719639a94e5c3dbdbc2
maleo <maleo@google.com>
2025-11-04 03:47:47 -0800

    SDK update
    GitOrigin-RevId: c6e1cdd3b15682ce3ac818f43f2241fb56bbf734

--- intrinsic/util/path_resolver/path_resolver.py
+++ intrinsic/util/path_resolver/path_resolver.py
@@ -7,11 +7,14 @@ import os
 from python.runfiles import runfiles
 
 _repo_name = 'ai_intrinsic_sdks'
+def rlocation(path: str) -> str:
+  """Resolves the runfiles path for the given rlocationpath."""
+  return os.path.normpath(
+      runfiles.Create().Rlocation(path),
+  )
 def resolve_runfiles_path(path: str) -> str:
   """Returns the runfiles path for the given path."""
   if os.path.isabs(path):
     return path
 
-  return os.path.normpath(
-      runfiles.Create().Rlocation(os.path.join(_repo_name, path)),
-  )
+  return rlocation(os.path.join(_repo_name, path))
