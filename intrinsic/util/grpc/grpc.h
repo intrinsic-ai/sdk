5cda65b2b2dc9d0bbbd6a8298439d62db134fe46
nickcox <nickcox@google.com>
2025-11-04 14:20:39 -0800

    SDK update
    GitOrigin-RevId: de1beadb85940f4be37ba6474ee63b7359a88b34

--- intrinsic/util/grpc/grpc.h
+++ intrinsic/util/grpc/grpc.h
@@ -17,7 +17,6 @@
 #include "intrinsic/connect/cc/grpc/channel.h"  // IWYU pragma: export
 
 namespace intrinsic {
-using ::intrinsic::connect::kGrpcClientConnectDefaultTimeout;
 using ::intrinsic::connect::UnlimitedMessageSizeGrpcChannelArgs;
 using ::intrinsic::connect::WaitForChannelConnected;
 
