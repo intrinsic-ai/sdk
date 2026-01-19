8ba8c6c7bd5b0a7350766c04ff6e5396ce49107c
Markus Ongyerth <ongyerth@google.com>
2026-01-19 14:18:19 +0100

    SDK update
    GitOrigin-RevId: 33e9d2bb3b67a66c28ad6c98a2e9f493193720a0

---
+++ intrinsic/tools/inctl/cmd/cluster/cluster_poweroff.go
@@ -0,0 +1,54 @@
+// Copyright 2023 Intrinsic Innovation LLC
+
+package cluster
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+
+	clustermanagergrpcpb "intrinsic/frontend/cloud/api/v1/clustermanager_api_go_grpc_proto"
+	clustermanagerpb "intrinsic/frontend/cloud/api/v1/clustermanager_api_go_grpc_proto"
+)
+
+var poweroffDesc = `
+Power off a cluster
+
+Example:
+	inctl cluster poweroff --cluster <my-cluster> --org <my-org>
+
+If the IPC is online, it will power off.
+`
+
+func poweroffCluster(ctx context.Context, conn *grpc.ClientConn, cluster string) error {
+	client := clustermanagergrpcpb.NewClustersServiceClient(conn)
+	if _, err := client.PoweroffCluster(
+		ctx, &clustermanagerpb.PoweroffClusterRequest{ClusterName: cluster}); err != nil {
+		return fmt.Errorf("request to poweroff cluster: %w", err)
+	}
+
+	return nil
+}
+
+var clusterPoweroffCmd = &cobra.Command{
+	Use:   "poweroff --cluster <my-cluster> --org <my-org>",
+	Short: "Power off an IPC",
+	Long:  poweroffDesc,
+	RunE: func(cmd *cobra.Command, argv []string) error {
+		ctx := cmd.Context()
+		conn, err := newCloudConn(ctx)
+		if err != nil {
+			return err
+		}
+		defer conn.Close()
+
+		return poweroffCluster(ctx, conn, clusterName)
+	},
+}
+
+func init() {
+	ClusterCmd.AddCommand(clusterPoweroffCmd)
+	clusterPoweroffCmd.PersistentFlags().StringVar(&clusterName, "cluster", "", "Name of the cluster to use")
+}
