f4f7405c08de9d9ff8cbe22bc8d562fabaa4940d
gsuta <79242525+gsuta@users.noreply.github.com>
2026-01-28 00:16:42 -0800

    SDK update
    GitOrigin-RevId: 6f8f45b12363f626a0613562d1a4ad7f12363c34

---
+++ intrinsic/kubernetes/acl/cc/header_names.h
@@ -0,0 +1,12 @@
+// Copyright 2023 Intrinsic Innovation LLC
+
+#ifndef INTRINSIC_KUBERNETES_ACL_CC_HEADER_NAMES_H_
+#define INTRINSIC_KUBERNETES_ACL_CC_HEADER_NAMES_H_
+
+namespace intrinsic::acl {
+
+inline constexpr char kOrganizationHeaderName[] = "x-intrinsic-org";
+
+}  // namespace intrinsic::acl
+
+#endif  // INTRINSIC_KUBERNETES_ACL_CC_HEADER_NAMES_H_
