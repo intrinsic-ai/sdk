579efe120962eec8095719d19210e67fb54ceaeb
Alex Faxå <alexanderfaxa@google.com>
2026-04-22 11:52:08 +0200

    SDK update
    GitOrigin-RevId: eb143ce0433518f52269cbc0eaa71ae3d8a62459

--- intrinsic/icon/release/portable/init_intrinsic.cc
+++ intrinsic/icon/release/portable/init_intrinsic.cc
@@ -1,5 +1,8 @@
 // Copyright 2023 Intrinsic Innovation LLC
 
+#include <signal.h>
+#include <stddef.h>
+
 #include <cstring>
 
 #include "absl/debugging/failure_signal_handler.h"
@@ -31,6 +34,14 @@ void InitIntrinsic(const char* usage, int argc, char* argv[]) {
   absl::FailureSignalHandlerOptions options;
   options.call_previous_handler = true;
   absl::InstallFailureSignalHandler(options);
+  // Restore the default SIGTERM handler. This is to avoid getting confusing
+  // stack traces printed when we purposefully kill subprocesses.
+  struct sigaction action;
+  action.sa_handler = SIG_DFL;        // Set to Default
+  sigemptyset(&action.sa_mask);       // Don't block any other signals
+  action.sa_flags = 0;                // No special flags
+  sigaction(SIGTERM, &action, NULL);  // Apply the change
+
   intrinsic::RtLogInitForThisThread();
 
   if (absl::GetFlag(FLAGS_sleep)) {
