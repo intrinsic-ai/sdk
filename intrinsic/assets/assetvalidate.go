2d094ba39ae52af7651d61276c084321336b2e57
Alex Schlegel <alexschlegel@google.com>
2026-05-19 13:55:02 -0700

    SDK update
    GitOrigin-RevId: 1d63da2885680df6134416c22403537ed7c37c9b

---
+++ intrinsic/assets/assetvalidate.go
@@ -0,0 +1,134 @@
+// Copyright 2023 Intrinsic Innovation LLC
+
+// Package assetvalidate provides utils for validating Assets.
+package assetvalidate
+
+import (
+	"fmt"
+
+	"intrinsic/assets/data/datavalidate"
+	"intrinsic/assets/hardware_devices/hardwaredevicevalidate"
+	"intrinsic/assets/idutils"
+	"intrinsic/assets/processes/processvalidate"
+	"intrinsic/assets/scene_objects/sceneobjectvalidate"
+	"intrinsic/assets/services/servicevalidate"
+	"intrinsic/skills/skillvalidate"
+
+	atypepb "intrinsic/assets/proto/asset_type_go_proto"
+	assetpb "intrinsic/assets/proto/v1/asset_go_proto"
+	processedassetpb "intrinsic/assets/proto/v1/processed_asset_go_proto"
+	rpb "intrinsic/assets/proto/v1/reference_go_proto"
+)
+
+type assetOptions struct {
+	dataOptions           []datavalidate.DataAssetOption
+	hardwareDeviceOptions []hardwaredevicevalidate.ProcessedHardwareDeviceManifestOption
+	processOptions        []processvalidate.ProcessAssetOption
+	sceneObjectOptions    []sceneobjectvalidate.ProcessedSceneObjectManifestOption
+	serviceOptions        []servicevalidate.ProcessedServiceManifestOption
+	skillOptions          []skillvalidate.ProcessedSkillManifestOption
+}
+
+// AssetOption is an option for validating an Asset.
+type AssetOption func(*assetOptions)
+
+// WithDataOptions appends options to use for validating Data Assets.
+//
+// Options will also apply to Data Assets in HardwareDevices.
+func WithDataOptions(options ...datavalidate.DataAssetOption) AssetOption {
+	return func(opts *assetOptions) {
+		opts.dataOptions = append(opts.dataOptions, options...)
+	}
+}
+
+// WithHardwareDeviceOptions appends options to use for validating HardwareDevices.
+func WithHardwareDeviceOptions(options ...hardwaredevicevalidate.ProcessedHardwareDeviceManifestOption) AssetOption {
+	return func(opts *assetOptions) {
+		opts.hardwareDeviceOptions = append(opts.hardwareDeviceOptions, options...)
+	}
+}
+
+// WithProcessOptions appends options to use for validating Processes.
+func WithProcessOptions(options ...processvalidate.ProcessAssetOption) AssetOption {
+	return func(opts *assetOptions) {
+		opts.processOptions = append(opts.processOptions, options...)
+	}
+}
+
+// WithSceneObjectOptions appends options to use for validating SceneObjects.
+//
+// Options will also apply to SceneObjects in HardwareDevices.
+func WithSceneObjectOptions(options ...sceneobjectvalidate.ProcessedSceneObjectManifestOption) AssetOption {
+	return func(opts *assetOptions) {
+		opts.sceneObjectOptions = append(opts.sceneObjectOptions, options...)
+	}
+}
+
+// WithServiceOptions appends options to use for validating Services.
+//
+// Options will also apply to Services in HardwareDevices.
+func WithServiceOptions(options ...servicevalidate.ProcessedServiceManifestOption) AssetOption {
+	return func(opts *assetOptions) {
+		opts.serviceOptions = append(opts.serviceOptions, options...)
+	}
+}
+
+func WithSkillOptions(options ...skillvalidate.ProcessedSkillManifestOption) AssetOption {
+	return func(opts *assetOptions) {
+		opts.skillOptions = append(opts.skillOptions, options...)
+	}
+}
+
+// Asset validates an Asset.
+func Asset(asset *assetpb.Asset, options ...AssetOption) error {
+	opts := &assetOptions{}
+	for _, opt := range options {
+		opt(opts)
+	}
+
+	if asset == nil {
+		return fmt.Errorf("Asset must not be nil")
+	}
+
+	switch src := asset.GetSource().(type) {
+	case *assetpb.Asset_Catalog:
+		return catalogAsset(src.Catalog)
+	case *assetpb.Asset_Local:
+		switch v := src.Local.GetVariant().(type) {
+		case *processedassetpb.ProcessedAsset_Data:
+			return datavalidate.DataAsset(v.Data, opts.dataOptions...)
+		case *processedassetpb.ProcessedAsset_HardwareDevice:
+			hwdOpts := append([]hardwaredevicevalidate.ProcessedHardwareDeviceManifestOption{
+				hardwaredevicevalidate.WithDataAssetOptions(opts.dataOptions...),
+				hardwaredevicevalidate.WithSceneObjectOptions(opts.sceneObjectOptions...),
+				hardwaredevicevalidate.WithServiceOptions(opts.serviceOptions...),
+			}, opts.hardwareDeviceOptions...)
+
+			return hardwaredevicevalidate.ProcessedHardwareDeviceManifest(v.HardwareDevice, hwdOpts...)
+		case *processedassetpb.ProcessedAsset_Process:
+			return processvalidate.ProcessAsset(v.Process, opts.processOptions...)
+		case *processedassetpb.ProcessedAsset_SceneObject:
+			return sceneobjectvalidate.ProcessedSceneObjectManifest(v.SceneObject, opts.sceneObjectOptions...)
+		case *processedassetpb.ProcessedAsset_Service:
+			return servicevalidate.ProcessedServiceManifest(v.Service, opts.serviceOptions...)
+		case *processedassetpb.ProcessedAsset_Skill:
+			return skillvalidate.ProcessedSkillManifest(v.Skill, opts.skillOptions...)
+		default:
+			return fmt.Errorf("unknown local Asset variant: %T", v)
+		}
+	default:
+		return fmt.Errorf("unknown Asset source %T", src)
+	}
+}
+
+func catalogAsset(ca *rpb.CatalogAsset) error {
+	if ca.GetAssetType() == atypepb.AssetType_ASSET_TYPE_UNSPECIFIED {
+		return fmt.Errorf("asset type must be specified")
+	}
+
+	if err := idutils.ValidateIDVersionProto(ca.GetIdVersion()); err != nil {
+		return fmt.Errorf("invalid id version: %w", err)
+	}
+
+	return nil
+}
