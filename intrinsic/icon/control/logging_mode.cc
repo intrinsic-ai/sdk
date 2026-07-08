666159750b90d33aec2a0771617fae6d11aea3fe
Filippos Sotiropoulos <sotiropoulos@google.com>
2026-07-08 16:01:07 +0200

    SDK update
    GitOrigin-RevId: 5ad4f2529f09404cdfb2fe1e830e2ffab823df35

--- intrinsic/icon/control/logging_mode.cc
+++ intrinsic/icon/control/logging_mode.cc
@@ -2,6 +2,10 @@
 
 #include "intrinsic/icon/control/logging_mode.h"
 
+#include <cmath>
+#include <cstdint>
+
+#include "absl/status/statusor.h"
 #include "intrinsic/icon/proto/logging_mode.pb.h"
 
 namespace intrinsic::icon {
@@ -25,6 +29,26 @@ intrinsic_proto::icon::LoggingMode ToProto(LoggingMode logging_mode) {
     default:
       return intrinsic_proto::icon::LoggingMode::LOGGING_MODE_THROTTLED;
   }
+}
+
+absl::StatusOr<int> CalculateDecimationFactor(double control_frequency_hz,
+                                              double target_log_rate_hz) {
+  if (control_frequency_hz <= 0.0 || target_log_rate_hz <= 0.0) {
+    return absl::InvalidArgumentError(
+        "control_frequency_hz and target_rate_hz must be positive.");
+  }
+  if (control_frequency_hz <= target_log_rate_hz) {
+    return 1;
+  }
+  return static_cast<int>(
+      std::round(control_frequency_hz / target_log_rate_hz));
+}
+
+bool IsThrottledLogCycle(uint64_t cycle, int decimation_factor) {
+  if (decimation_factor <= 1) {
+    return true;
+  }
+  return (cycle % decimation_factor) == 0;
 }
 
 }  // namespace intrinsic::icon
--- intrinsic/icon/control/logging_mode.h
+++ intrinsic/icon/control/logging_mode.h
@@ -3,6 +3,10 @@
 #ifndef INTRINSIC_ICON_CONTROL_LOGGING_MODE_H_
 #define INTRINSIC_ICON_CONTROL_LOGGING_MODE_H_
 
+#include <cstdint>
+
+#include "absl/status/statusor.h"
+#include "absl/time/time.h"
 #include "intrinsic/icon/proto/logging_mode.pb.h"
 
 namespace intrinsic::icon {
@@ -14,9 +18,23 @@ enum class LoggingMode : char {
   kFullRate,
 };
 
+// Regardless of the actual controller frequency, we will publish a throttled
+// version on a different topic that is always 50 Hz to simplify its use in
+// subscribers that do not need or want the full-rate (often >= 500 Hz) stream
+// of status messages. Details in go/intrinsic-robot-status-rate
+constexpr double kThrottledStatusRate = 50.0;
+
 LoggingMode FromProto(const intrinsic_proto::icon::LoggingMode& proto);
 
 intrinsic_proto::icon::LoggingMode ToProto(LoggingMode mode);
+
+// Calculates the factor with which to downsample cloud logs in ICON.
+absl::StatusOr<int> CalculateDecimationFactor(double control_frequency_hz,
+                                              double target_log_rate_hz);
+
+// Calulates whether the current `cycle` should be published while throttling
+// the logging rate.
+bool IsThrottledLogCycle(uint64_t cycle, int decimation_factor);
 
 }  // namespace intrinsic::icon
 
