fc7610991046f5c7cc03c1a9ea8b6846cb4f51d6
Marco Feuerstein <feuer@google.com>
2026-09-01 11:29:27 +0200

    SDK update
    GitOrigin-RevId: 298b9b9c96f61e626563418cc87a14361c80e5bc

---
+++ intrinsic/math/units.h
@@ -0,0 +1,39 @@
+// Copyright 2026 Intrinsic Innovation LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef INTRINSIC_MATH_UNITS_H_
+#define INTRINSIC_MATH_UNITS_H_
+
+#include <concepts>
+#include <numbers>
+
+namespace intrinsic {
+
+// Converts an angle from radians to degrees.
+template <std::floating_point T>
+[[nodiscard]] constexpr T RadToDeg(T rad) noexcept {
+  constexpr T kDegPerRad = T{180} / std::numbers::pi_v<T>;
+  return rad * kDegPerRad;
+}
+
+// Converts an angle from degrees to radians.
+template <std::floating_point T>
+[[nodiscard]] constexpr T DegToRad(T deg) noexcept {
+  constexpr T kRadPerDeg = std::numbers::pi_v<T> / T{180};
+  return deg * kRadPerDeg;
+}
+
+}  // namespace intrinsic
+
+#endif  // INTRINSIC_MATH_UNITS_H_
