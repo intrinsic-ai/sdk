f93f4f5a0082578374af58de677f49290a9fb001
gsuta <79242525+gsuta@users.noreply.github.com>
2026-09-21 16:21:25 -0700

    - No-op.
    RELNOTES=NONE
    TESTED=AUTO
    GitOrigin-RevId: 4028331ae8d20e12991a5ce0d5232b9557dc3e14

--- intrinsic/motion_planning/motion_planner_client.cc
+++ intrinsic/motion_planning/motion_planner_client.cc
@@ -164,7 +164,6 @@ MotionPlannerClient::PlanTrajectory(
   result.lock_motion_id = response.has_lock_motion_id()
                               ? std::optional(response.lock_motion_id())
                               : std::nullopt;
-  result.logging_id = response.logging_id();
   return result;
 }
 
@@ -190,7 +189,6 @@ MotionPlannerClient::PlanPath(
   result.swept_volume.insert(result.swept_volume.begin(),
                              response.swept_volume().begin(),
                              response.swept_volume().end());
-  result.logging_id = response.logging_id();
   return result;
 }
 
@@ -266,7 +264,6 @@ MotionPlannerClient::ComputeIk(
   ComputeIkResult result;
   result.solutions = ToVectorXds(response.solutions());
   result.ik_debug_information = response.ik_debug_information();
-  result.logging_id = response.logging_id();
   return result;
 }
 
